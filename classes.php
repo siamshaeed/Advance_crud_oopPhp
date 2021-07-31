a38316da7b2df5d1168454be6bad4d97a3c9fbff
siamshaeed <siamshaeed@gmail.com>
2021-07-31 13:54:01 +0600

    code formate

--- classes.php
+++ classes.php
@@ -1,22 +1,20 @@
 <?php
-    //oop class (student insert).
-    class StudentInfo{
+    
+    class StudentInfo{//oop class (for student insert).
         //database connection make global using __construct function.
         protected $db; //make global veriable
         public function __construct(){
             //Database connect
             $this->db = new mysqli("localhost", "root", "", "oopCrud_php");
         }
-        //oop method
-        public function studentInsert($data){
+        public function studentInsert($data){   //oop method
             //mysql query
             $sql = "INSERT INTO stuinfo(`stuId`, `stuName`, `stuEmail`, `stuPhone`, `stuDept`) VALUES ('', '$data[stuName]', '$data[stuEmail]', '$data[stuPhone]', '$data[stuDept]')";
 
             //query exicute
             // mysqli_query($db, $sql);
 
-            //query exicute chack
-            if(mysqli_query($this->db, $sql)){
+            if(mysqli_query($this->db, $sql)){ //query exicute chack
                 $message = "Data insert Done";
                 return $message;
             }else{
@@ -24,4 +22,3 @@
             }
         } 
     }
-?>
