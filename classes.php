f8aca9b4a0969ff63464e08897f2ec4330ea7c92
siamshaeed <siamshaeed@gmail.com>
2021-07-31 13:51:38 +0600

    database make global

--- classes.php
+++ classes.php
@@ -1,22 +1,26 @@
 <?php
     //oop class (student insert).
     class StudentInfo{
+        //database connection make global using __construct function.
+        protected $db; //make global veriable
+        public function __construct(){
+            //Database connect
+            $this->db = new mysqli("localhost", "root", "", "oopCrud_php");
+        }
         //oop method
         public function studentInsert($data){
-           //Database connect
-            $db = new mysqli("localhost", "root", "", "oopCrud_php");
             //mysql query
             $sql = "INSERT INTO stuinfo(`stuId`, `stuName`, `stuEmail`, `stuPhone`, `stuDept`) VALUES ('', '$data[stuName]', '$data[stuEmail]', '$data[stuPhone]', '$data[stuDept]')";
-            
+
             //query exicute
             // mysqli_query($db, $sql);
 
             //query exicute chack
-            if(mysqli_query($db, $sql)){
+            if(mysqli_query($this->db, $sql)){
                 $message = "Data insert Done";
                 return $message;
             }else{
-                die('Query Exicute Problem'. mysqli_error($db));
+                die('Query Exicute Problem'. mysqli_error($this->db));
             }
         } 
     }
