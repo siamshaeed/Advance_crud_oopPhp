6f6ce7d550f0a14c904e702a161e9d4820d22db4
siamshaeed <siamshaeed@gmail.com>
2021-08-23 23:46:42 +0600

    update commenting

--- update.php
+++ update.php
@@ -1,8 +1,3 @@
-<?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$db_connect = new mysqli("localhost", "root", '', "php-crud");
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
